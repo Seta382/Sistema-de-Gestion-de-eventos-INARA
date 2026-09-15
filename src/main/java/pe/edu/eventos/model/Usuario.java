2fa42fac3bf72b71d4ea37880da649cf5c685e91
Seta382 <sebastiantucta@gmail.com>
2026-09-15 18:55:48 -0500

    Create Usuario class for user management
    Added Usuario class with attributes and methods for user management.

--- src/main/java/pe/edu/eventos/model/Usuario.java
+++ src/main/java/pe/edu/eventos/model/Usuario.java
@@ -1,1 +1,73 @@
+package main.java.pe.edu.eventos.model;
 
+/**
+ *Para los clientes y organizadores que interactuen con el sistema
+ */
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String password;
+    private String rol;
+
+    public Usuario(){
+    }
+
+    public Usuario(Integer id, String nombre, String apellido, String correo, String password, String rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
